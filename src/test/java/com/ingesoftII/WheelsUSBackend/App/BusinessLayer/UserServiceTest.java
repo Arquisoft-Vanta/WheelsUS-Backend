3b2d5bc4fd8b524ceec9c67cd20d420d6a664121
Cristian Sainea <cesaineam@unal.edu.co>
2020-10-01 19:48:01 -0500

    intento de test

---
+++ src/test/java/com/ingesoftII/WheelsUSBackend/App/BusinessLayer/UserServiceTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingesoftII.WheelsUSBackend.App.BusinessLayer;
+
+import App.BusinessLayer.Repositories.UserRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserServiceTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository employeeRepository;
+
+}
