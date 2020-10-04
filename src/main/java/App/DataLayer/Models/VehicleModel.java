68d96b6bfe6f980970fe8c5f79091d785d659dc5
Cesar David Pineda Osorio <cdpinedao@unal.edu.co>
2020-10-04 00:35:19 -0500

    ModificacionTablaVehiculos

--- src/main/java/App/DataLayer/Models/VehicleModel.java
+++ src/main/java/App/DataLayer/Models/VehicleModel.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
+import java.sql.Date;
 
 /**
  *
@@ -30,7 +31,7 @@ public class VehicleModel{
 
     }
     
-    public VehicleModel(int vehicleOwner, String vehicleLicenseplate, int vehicleType, String vehicleModel, int vehicleYear, String vehicleColor, LocalDateTime vehicleRegistryDatetime, String vehiclePicture, int vehicleCapacity){
+    public VehicleModel(int vehicleOwner, String vehicleLicenseplate, int vehicleType, String vehicleModel, int vehicleYear, String vehicleColor, LocalDateTime vehicleRegistryDatetime, String vehiclePicture, int vehicleCapacity, String vehicleBrand, String vehicleServiceType, String vehicleBody, Date vehicleSoatExpiration, int vehicleEngine, String vehicleGasType){
     
         this.vehicleOwner = vehicleOwner;
         this.vehicleLicenseplate = vehicleLicenseplate;
@@ -41,10 +42,16 @@ public class VehicleModel{
         this.vehicleRegistryDatetime = vehicleRegistryDatetime;
         this.vehiclePicture = vehiclePicture;
         this.vehicleCapacity = vehicleCapacity;
+        this.vehicleBrand = vehicleBrand;
+        this.vehicleServiceType = vehicleServiceType;
+        this.vehicleBody = vehicleBody;
+        this.vehicleSoatExpiration = vehicleSoatExpiration;
+        this.vehicleEngine = vehicleEngine;
+        this.vehicleGasType = vehicleGasType;
         
     }
     
-    public VehicleModel(int idVehicle, int vehicleOwner, String vehicleLicenseplate, int vehicleType, String vehicleModel, int vehicleYear, String vehicleColor, LocalDateTime vehicleRegistryDatetime, String vehiclePicture, int vehicleCapacity){
+    public VehicleModel(int idVehicle, int vehicleOwner, String vehicleLicenseplate, int vehicleType, String vehicleModel, int vehicleYear, String vehicleColor, LocalDateTime vehicleRegistryDatetime, String vehiclePicture, int vehicleCapacity, String vehicleBrand, String vehicleServiceType, String vehicleBody, Date vehicleSoatExpiration, int vehicleEngine, String vehicleGasType){
         this.idVehicle = idVehicle;
         this.vehicleOwner = vehicleOwner;
         this.vehicleLicenseplate = vehicleLicenseplate;
@@ -55,6 +62,12 @@ public class VehicleModel{
         this.vehicleRegistryDatetime = vehicleRegistryDatetime;
         this.vehiclePicture = vehiclePicture;
         this.vehicleCapacity = vehicleCapacity;
+        this.vehicleBrand = vehicleBrand;
+        this.vehicleServiceType = vehicleServiceType;
+        this.vehicleBody = vehicleBody;
+        this.vehicleSoatExpiration = vehicleSoatExpiration;
+        this.vehicleEngine = vehicleEngine;
+        this.vehicleGasType = vehicleGasType;
         
     }
     
@@ -82,5 +95,17 @@ public class VehicleModel{
     private String vehiclePicture;
     
     private int vehicleCapacity;
+    
+    private String vehicleBrand;
+    
+    private String vehicleServiceType;
+    
+    private String vehicleBody;
+    
+    private Date vehicleSoatExpiration;
+    
+    private int vehicleEngine;
+    
+    private String vehicleGasType;
     
 }
