3e828947b60402d56489d586a0b9c8db8ee63dde
Gonzalo Diaz <egonzalodm@gmail.com>
2020-10-15 18:12:17 -0500

    Creacion de los POJOS de vehicle y user

---
+++ src/main/java/App/BusinessLayer/Pojo/UserPOJO.java
@@ -0,0 +1,117 @@
+package App.BusinessLayer.Pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class UserPOJO {
+    private int idUser;
+
+    private String userName;
+
+    private String userDoc;
+
+    private String userPhone;
+
+    private int universityId;
+
+    private String userMail;
+
+    private String userAddress;
+
+    private String password;
+
+    private LocalDateTime registryDatetime;
+
+    private String picture;
+
+    private String Rh;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserDoc() {
+        return userDoc;
+    }
+
+    public void setUserDoc(String userDoc) {
+        this.userDoc = userDoc;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(int universityId) {
+        this.universityId = universityId;
+    }
+
+    public String getUserMail() {
+        return userMail;
+    }
+
+    public void setUserMail(String userMail) {
+        this.userMail = userMail;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getRegistryDatetime() {
+        return registryDatetime;
+    }
+
+    public void setRegistryDatetime(LocalDateTime registryDatetime) {
+        this.registryDatetime = registryDatetime;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getRh() {
+        return Rh;
+    }
+
+    public void setRh(String rh) {
+        Rh = rh;
+    }
+}
